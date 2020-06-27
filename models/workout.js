b9346e263fe060ec2c0ad6dd0376a907bff0908a
Brandon P <brandonj.peart1@gmail.com>
2020-06-26 20:09:34 -0400

    update the files

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,47 @@
-module.exports = {
-    workout: require(`./workout`)
-};
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    exercise: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "an exercise must be selected?",
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "what is the name of the exercise?",
+            },
+            duration: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number,
+                required: true,
+            },
+            reps: {
+                type: Number,
+                required: true,
+            },
+            sets: {
+                type: Number,
+                required: true,
+            },
+            distance: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+});
+
+const workout = mongoose.model("exercise", exerciseSchema);
+
+module.exports = workout;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 4000;
 
@@ -10,13 +11,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
+app.use(require("./routes/workout"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
